4b42dbdd0156d82caa8a743f80abdc5552bad664
imkost <imkost@yandex.ru>
2017-04-06 16:48:23 +0300

    update

--- befall.js
+++ befall.js
@@ -1,73 +1,61 @@
-function befall(_isKinded) {
-  var isKinded = _isKinded === true ? true : false;
-  var kinds = {};
+function befall() {
+  var fns = [];
+  var b = emitter.bind(fns);
+  b.off = off.bind(fns);
 
-  function emitter() {
-    var args = extractArgs(arguments);
-    var kind = args.kind;
-    var params = args.params;
+  return b;
+}
 
-    if (typeof params[0] === 'function') {
-      register(kind, params[0]);
-    } else {
-      return fire(kind, params);
-    }
-  }
+function befallKind() {
+  var emitters = {};
 
-  emitter.off = function() {
-    var args = extractArgs(arguments);
-    off(args.kind, args.params);
+  return function(kind) {
+    emitters[kind] || (emitters[kind] = befall());
   };
+}
 
-  emitter.fire = fire;
+function emitter() {
+  var fns = this;
+  var args = arguments;
+  var first = args[0];
 
-  function extractArgs(args) {
-    var kind;
-    var params;
-
-    if (isKinded) {
-      kind = args[0];
-      params = Array.prototype.slice.call(args, 1);
-    } else {
-      kind = '_';
-      params = args;
-    }
-
-    return { kind: kind, params: params };
-  }
-
-  function register(kind, fn) {
-    (kinds[kind] || (kinds[kind] = [])).push(fn);
-  }
-
-  function fire(kind, params) {
-    var fns = kinds[kind] || [];
+  if (typeof first === 'function') {
+    fns.push(first);
+  } else {
     var fnsCount = fns.length;
     var prevent = false;
-    var i;
 
-    for (i = 0; i < fnsCount; i++) {
-      if (fns[i].apply(null, params)) {
+    for (var i = 0; i < fnsCount; i++) {
+      if (fns[i].apply(null, args) === true) {
         prevent = true;
       }
     }
 
     return prevent;
-  };
-
-  function off(kind, fn) {
-    removeFromArray(kinds[kind], fn);
   }
+}
 
-  return emitter;
+function off(fn) {
+  var fns = this;
+
+  if (fn) {
+    removeFromArray(fns, fn);
+  } else {
+    fns.length = 0;
+  }
 }
 
 function removeFromArray(array, value) {
-  var index = array.indexOf(value);
+  while (true) {
+    var index = array.indexOf(value);
 
-  if (index >= 0) {
-    array.splice(index, 1);
+    if (index >= 0) {
+      array.splice(index, 1);
+    } else {
+      break;
+    }
   }
 }
 
 module.exports = befall;
+module.exports.befallKind = befallKind;
